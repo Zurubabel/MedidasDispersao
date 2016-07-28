baf6257cf537393d25643788a2627f6529a98751
Aulas <Aulas>
2016-07-28 16:45:23 -0300

    Criação do método de dispersão em array

--- java/src/dispersao/MedidasDispersao.java
+++ java/src/dispersao/MedidasDispersao.java
@@ -8,30 +8,56 @@ public class MedidasDispersao {
 	// GITHUB - https://github.com/Zurubabel/MedidasDispersao
 	
 	public static void main(String[] args) {
+		List<Integer> numerosLista = new ArrayList<>();
 		
-		List<Integer> numeros = new ArrayList<>();
+		numerosLista.add(1);
+		numerosLista.add(1);
+		numerosLista.add(2);
+		numerosLista.add(2);
+		numerosLista.add(2);
+		numerosLista.add(3);
+		numerosLista.add(3);
+		numerosLista.add(3);
+		numerosLista.add(3);
+		numerosLista.add(4);
+		numerosLista.add(4);
+		numerosLista.add(4);
+		numerosLista.add(5);
+		numerosLista.add(5);
 		
-		numeros.add(1);
-		numeros.add(1);
-		numeros.add(2);
-		numeros.add(2);
-		numeros.add(2);
-		numeros.add(3);
-		numeros.add(3);
-		numeros.add(3);
-		numeros.add(3);
-		numeros.add(4);
-		numeros.add(4);
-		numeros.add(4);
-		numeros.add(5);
-		numeros.add(5);
+		exibirAlcanceLista(numerosLista);
 		
-		exibirAlcance(numeros);
+		// ----------------------
+		
+		int[] numerosArray = {20,2,2,2,3,3,3,3,4,4,4,5,5};
+		exibirAlcanceArray(numerosArray);
+		
+		
 
 	}
 	
-	// Alcance - Aula 1
-	public static void exibirAlcance(List<Integer> numeros) {
+	// Alcance - Aula 3
+	public static void exibirAlcanceArray(int[] numeros) {
+		int maiorNumero = 0 ;
+		int menorNumero = numeros[0];
+		
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] < menorNumero) {
+				// É o menor número
+				menorNumero = numeros[i];
+			}
+			
+			if (numeros[i] > maiorNumero) {
+				maiorNumero = numeros[i];
+			}
+		}
+		System.out.println("--- exibirAlcanceArray ---");
+		System.out.println("Maior numero : " + maiorNumero + " | menor numero : " + menorNumero);
+		System.out.println("Alcance - " + (maiorNumero - menorNumero));
+	}
+	
+	// Alcance - Aula 2
+	public static void exibirAlcanceLista(List<Integer> numeros) {
 		// Cálculo - Maior número - menor
 		
 		int maiorNumero = 0 ;
@@ -47,7 +73,7 @@ public class MedidasDispersao {
 				maiorNumero = numero;
 			}
 		}
-		
+		System.out.println("--- exibirAlcanceLista ---");
 		System.out.println("Maior numero : " + maiorNumero + " | menor numero : " + menorNumero);
 		System.out.println("Alcance - " + (maiorNumero - menorNumero));
 	}
